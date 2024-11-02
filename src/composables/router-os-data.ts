1b1d6040262002a2c7851ae0b3d6c8ded0238e00
jjjjose <jjjjoseignacio@gmail.com>
2024-11-02 06:35:07 -0400

    add prettier formated

--- src/composables/router-os-data.ts
+++ src/composables/router-os-data.ts
@@ -1,38 +1,38 @@
-import {reactive, toRefs} from "vue";
+import { reactive, toRefs } from 'vue'
 
 interface RouterOsDataState {
-    chapId: string;
-    chapSecret: string;
-    ip: string;
-    chapChallenge: string;
-    linkLoginOnly: string;
-    linkOrig: string;
+  chapId: string;
+  chapSecret: string;
+  ip: string;
+  chapChallenge: string;
+  linkLoginOnly: string;
+  linkOrig: string;
 }
 
 function createRouterOsData() {
-    const state = reactive<RouterOsDataState>({
-        chapId: '',
-        chapSecret: '',
-        ip: '',
-        chapChallenge: '',
-        linkLoginOnly: '',
-        linkOrig: ''
-    });
+  const state = reactive<RouterOsDataState>({
+    chapId: '',
+    chapSecret: '',
+    ip: '',
+    chapChallenge: '',
+    linkLoginOnly: '',
+    linkOrig: '',
+  })
 
-    const saveData = (): void => {
-        const dataLocal = localStorage.getItem('dataRouterOS');
-        const dataRouterOS: RouterOsDataState = dataLocal ? JSON.parse(dataLocal) : alert('No hay datos guardados');
-        Object.assign(state, dataRouterOS);
-    };
+  const saveData = (): void => {
+    const dataLocal = localStorage.getItem('dataRouterOS')
+    const dataRouterOS: RouterOsDataState = dataLocal ? JSON.parse(dataLocal) : alert('No hay datos guardados')
+    Object.assign(state, dataRouterOS)
+  }
 
-    return {...toRefs(state), saveData};
+  return { ...toRefs(state), saveData }
 }
 
-let uniqueInstance: ReturnType<typeof createRouterOsData> | null = null;
+let uniqueInstance: ReturnType<typeof createRouterOsData> | null = null
 
 export default function useRouterOsData(): ReturnType<typeof createRouterOsData> {
-    if (!uniqueInstance) {
-        uniqueInstance = createRouterOsData();
-    }
-    return uniqueInstance;
+  if (!uniqueInstance) {
+    uniqueInstance = createRouterOsData()
+  }
+  return uniqueInstance
 }
