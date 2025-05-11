0d1013cddd57eda577a128971115aaffb940aa48
jjjjose <jjjjoseignacio@gmail.com>
2025-05-10 21:52:53 -0400

    all delete

--- src/main.ts
+++
@@ -1,6 +0,0 @@
-import { createApp } from 'vue'
-import './assets/index.css'
-import App from './App.vue'
-import router from './routes'
-
-createApp(App).use(router).mount('#app')
--- src/routes/routes.ts
+++
@@ -1,6 +0,0 @@
-const routes = [
-  // { path: '/', component: import('../components/Portal.vue') },
-  { path: '/', component: () => import('../components/Apaza.vue') },
-]
-
-export default routes
